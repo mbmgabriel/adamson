d2ec2a4b7aa1f22e52f7a1b8cf00111653595bef
Michael Ben Gabriel <michaelbengabriel@Michaels-iMac.example.com>
2022-11-24 10:46:06 +0800

    new design login

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -44,21 +44,21 @@ function Login() {
   return (
     // <Container fluid style={{height:'100vh'}}>
     // <Row style={{height:'100vh'}}>
-    //   <Col md={8} className='login-screen-left'>
+    //   <Col md={8} classNameName='login-screen-left'>
     //     <Image fluid style={{height:'100vh'}} src={bg}></Image>
     //   </Col>
-    //   <Col md={4} className='login-screen-right'>
+    //   <Col md={4} classNameName='login-screen-right'>
     //     <Row>
     //       <Col md={3}></Col>
     //       <Col md={6}>
     //       <div>
-    //       <h1 className="title"><span className="orange">Haru</span>Gabriel</h1>
-    //       {/* <p className="subtitle">Welcome back to TekTeach! Making learning, a great experience!</p> */}
+    //       <h1 classNameName="title"><span classNameName="orange">Haru</span>Gabriel</h1>
+    //       {/* <p classNameName="subtitle">Welcome back to TekTeach! Making learning, a great experience!</p> */}
     //         <Form onSubmit={login}>
     //           <Form.Group controlId="exampleForm.ControlInput1">
-    //             <Form.Label className="custom-label">E-mail / Username</Form.Label>
+    //             <Form.Label classNameName="custom-label">E-mail / Username</Form.Label>
     //             <Form.Control 
-    //               className="custom-input" 
+    //               classNameName="custom-input" 
     //               size="md" 
     //               type="text" 
     //               placeholder="Enter e-mail or username here"
@@ -66,20 +66,20 @@ function Login() {
     //             />
     //           </Form.Group>
     //           <Form.Group controlId="exampleForm.ControlInput1">
-    //             <Form.Label className="custom-label">Password</Form.Label>
+    //             <Form.Label classNameName="custom-label">Password</Form.Label>
     //             <InputGroup>
     //               <Form.Control 
-    //                 className="custom-input" 
+    //                 classNameName="custom-input" 
     //                 size="md" 
     //                 type={showPassword ? 'text' : "password"} 
     //                 placeholder="Enter password here"
     //                 onChange={(e) => setPassword(e.target.value)}
     //               />
-    //               <InputGroup.Text className='custom-input border-0'><i className={showPassword ? 'fa fa-eye' : 'fa fa-eye-slash'} onClick={()=> setShowPassword(!showPassword)}/></InputGroup.Text>
+    //               <InputGroup.Text classNameName='custom-input border-0'><i classNameName={showPassword ? 'fa fa-eye' : 'fa fa-eye-slash'} onClick={()=> setShowPassword(!showPassword)}/></InputGroup.Text>
     //             </InputGroup>
     //           </Form.Group>
-    //           <Link className="link-orange font-24 d-none" to="/forgot_password" >Forgot Password</Link>
-    //           <Button className="btn btn-md btn-primary btn-auth w-100 d-block mt-4 mb-4" size="md" variant="primary" type="submit">Log In</Button>
+    //           <Link classNameName="link-orange font-24 d-none" to="/forgot_password" >Forgot Password</Link>
+    //           <Button classNameName="btn btn-md btn-primary btn-auth w-100 d-block mt-4 mb-4" size="md" variant="primary" type="submit">Log In</Button>
     //         </Form>
     //       </div>
     //       </Col>
@@ -87,61 +87,103 @@ function Login() {
     //   </Col>
     // </Row>
     // </Container>
-
-<div className="container-fluid">
-<div className="auth-container">
-  <Row>
-    <Col md={8}>
-      <div className="login-container login-bg-image"></div>
-    </Col>
-    <Col md={4}>
-    <h1 className="title">VETDRUMS</h1>
-      <p className="subtitle" style={{fontWeight:"bold", color:"#0981D1"}}>Welcome! Veterrinary Drug Monitoring System</p>
-        <Form onSubmit={login}>
-          <Form.Group controlId="exampleForm.ControlInput1">
-            <Form.Label className="custom-label">Username</Form.Label>
-            <Form.Control 
-            className="custom-input" 
-            size="md" 
-            type="text" 
-            placeholder="Enter e-mail or username here"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId="exampleForm.ControlInput1">
-          <Form.Label className="custom-label">Password</Form.Label>
-          <InputGroup>
-            <Form.Control 
-              className="custom-input" 
+/* <>
+  <div classNameName="auth-container">
+    <Row>
+      <Col md={8}>
+        <div classNameName="login-container login-bg-image"></div>
+      </Col>
+      <Col md={4}>
+      <h1 classNameName="title">VETDRUMS</h1>
+        <p classNameName="subtitle" style={{fontWeight:"bold", color:"#0981D1"}}>Welcome! Veterrinary Drug Monitoring System</p>
+          <Form onSubmit={login}>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label classNameName="custom-label">Username</Form.Label>
+              <Form.Control 
+              classNameName="custom-input" 
               size="md" 
-              type={showPassword ? 'text' : "password"} 
-              placeholder="Enter password here"
-              onChange={(e) => setPassword(e.target.value)}
+              type="text" 
+              placeholder="Enter e-mail or username here"
+              onChange={(e) => setUsername(e.target.value)}
             />
-            <InputGroup.Text className='custom-input border-0'><i className={showPassword ? 'fa fa-eye' : 'fa fa-eye-slash'} onClick={()=> setShowPassword(!showPassword)}/></InputGroup.Text>
-          </InputGroup>
-        </Form.Group>
-        <Link className="link-orange font-24 d-none" to="/forgot_password" >Forgot Password</Link>
-        <Button className="btn btn-md btn-primary btn-auth w-100 d-block mt-4 mb-4" size="md" variant="primary" type="submit">Log In</Button>
-        <div>Don't have an account yet? <span style={{fontWeight:"bold"}}><a href="/register">Sign up here</a></span></div>
-      </Form>
-    </Col> 
-  </Row>
-</div>
-<ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-</div>
-
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlInput1">
+            <Form.Label classNameName="custom-label">Password</Form.Label>
+            <InputGroup>
+              <Form.Control 
+                classNameName="custom-input" 
+                size="md" 
+                type={showPassword ? 'text' : "password"} 
+                placeholder="Enter password here"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <InputGroup.Text classNameName='custom-input border-0'><i classNameName={showPassword ? 'fa fa-eye' : 'fa fa-eye-slash'} onClick={()=> setShowPassword(!showPassword)}/></InputGroup.Text>
+            </InputGroup>
+          </Form.Group>
+          <Link classNameName="link-orange font-24 d-none" to="/forgot_password" >Forgot Password</Link>
+          <Button classNameName="btn btn-md btn-primary btn-auth w-100 d-block mt-4 mb-4" size="md" variant="primary" type="submit">Log In</Button>
+          <div>Don't have an account yet? <span style={{fontWeight:"bold"}}><a href="/register">Sign up here</a></span></div>
+        </Form>
+      </Col> 
+    </Row>
+  </div>
+  <ToastContainer
+    position="top-right"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="light"
+  />
+</> */
+   <div className="container-fluid">
+     <div className="row" style={{display:"flex", minHeight:"100vh"}}>
+       <div className="col-lg-5 col-xl-4 d-none d-lg-flex justify-content-center align-items-center min-vh-lg-100 position-relative bg-light">
+         a
+       </div>
+       <div className="col-lg-7 col-xl-8 d-flex justify-content-center align-items-center min-vh-lg-100">
+          <div className="flex-grow-1 mx-auto" style={{maxWidth: '28rem'}}>
+            <div className="text-center mb-5 mb-md-7">
+              <h1 classNameName="title">VETDRUMS</h1>
+              <p classNameName="subtitle" style={{fontWeight:"bold", color:"#0981D1"}}>Welcome! Veterrinary Drug Monitoring System</p>
+            </div>
+                  <Form onSubmit={login}>
+                    <Form.Group controlId="exampleForm.ControlInput1">
+                      <Form.Label classNameName="custom-label">Username</Form.Label>
+                      <Form.Control 
+                      classNameName="custom-input" 
+                      size="md" 
+                      type="text" 
+                      placeholder="Enter e-mail or username here"
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Label classNameName="custom-label">Password</Form.Label>
+                    <InputGroup>
+                      <Form.Control 
+                        classNameName="custom-input" 
+                        size="md" 
+                        type={showPassword ? 'text' : "password"} 
+                        placeholder="Enter password here"
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                      <InputGroup.Text classNameName='custom-input border-0'><i classNameName={showPassword ? 'fa fa-eye' : 'fa fa-eye-slash'} onClick={()=> setShowPassword(!showPassword)}/></InputGroup.Text>
+                    </InputGroup>
+                  </Form.Group>
+                  <Link classNameName="link-orange font-24 d-none" to="/forgot_password" >Forgot Password</Link>
+                  <Button classNameName="btn btn-md btn-primary btn-auth w-100 d-block mt-4 mb-4" size="md" variant="primary" type="submit">Log In</Button>
+                  <div>Don't have an account yet? <span style={{fontWeight:"bold"}}><a href="/register">Sign up here</a></span></div>
+                </Form>
+            </div>
+          </div>
+       
+     </div>
+   </div>
   );
 }
 
